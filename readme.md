26f591f3efa16d9ddc7e32ec0de3083f4e43ba44
Sigurd Fosseng <sigurd@fosseng.net>
2023-09-21 19:42:39 +0200

    ok then...

---
+++ readme.md
@@ -0,0 +1,8 @@
+# multipart/form-data experiment
+
+This is a simple experiment to test the `multipart/form-data` encoding type, both as a request and response.
+
+```sh
+npm install
+npm start
+```
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multipart = require('connect-multiparty');
+const FormData = require('form-data');
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(multipart());
+
+app.post('/api', (req, res) => {
+  const form = new FormData();
+  form.append('hello', `from the server, you typed: ${req.body.hello}`);
+  res.setHeader(
+    'Content-Type',
+    'multipart/form-data; boundary=' + form.getBoundary()
+  );
+  form.pipe(res);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
